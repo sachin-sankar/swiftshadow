92132ac9d7d6efd8e8673df41898bb880da70241
Sachin Sankar <mail.sachinsankar@gmail.com>
2023-04-25 11:30:54 +0530

    Add files via upload

---
+++ swiftshadow/helpers.py
@@ -0,0 +1,26 @@
+from constants import CountryCodes
+from requests import get
+def getCountryCode(countryName):
+	try:
+		return CountryCodes[countryName]
+	except KeyError:
+		for name in list(CountryCodes.keys()):
+			if countryName in name:
+				return CountryCodes[name]
+				
+def checkProxy(proxy,countries):
+	if countries != []:
+		if proxy[-1].upper() not in countries:
+			return False
+	proxyDict = {proxy[1]:proxy[0]}
+	try:
+		resp = get(f'{proxy[1]}://ipinfo.io/ip',proxies=proxyDict,timeout=2).text
+		if resp.count('.') == 3:
+			return True
+		return False
+	except Exception as e:
+		#log('error',str(e))
+		return False
+
+def log(level,message):
+	print(f'[ swiftshadow ] {level.upper()} : {message} ')		
