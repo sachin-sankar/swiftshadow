6cf3b0c06dc25bbb8c5adaad35a7b16ab56fbe81
Sachin Sankar <mail.sachinsankar@gmail.com>
2023-04-25 11:29:25 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,22 +8,37 @@ from setuptools import setup, find_packages
 
 VERSION = {}
 
-with open("./swiftshadow/__init__.py") as fp:
+with open("./beesly/__init__.py") as fp:
+
     # pylint: disable=W0122
 
     exec(fp.read(), VERSION)
 
 setup(
+
     name="swiftshadow",
+
     author="Sachin Sankar",
+
     author_email="mail.sachinsankar@gmail.com",
+
     url="https://github.com/Chicken1Geek/swiftshadow",
+
     description="Free IP Proxy rotator for python",
-    long_description="Swiftshadow is a proxy rotator that sources proxies for free and provides elegant pythonic API to manage proxies. Build for speed and performance in mind.",
+    long_description = 'Swiftshadow is a proxy rotator that sources proxies for free and provides elegant pythonic API to manage proxies. Build for speed and performance in mind.',
+
     version=VERSION.get("__version__", "0.0.0"),
+
     packages=find_packages(where=".", exclude=["tests"]),
-    install_requires=["requests"],
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+
+    install_requires=[
+        "requests"
     ],
+
+    classifiers=[
+
+        "Development Status :: 2 - Pre-Alpha",
+
+    ],
+
 )
