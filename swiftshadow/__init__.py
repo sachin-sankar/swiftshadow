74a23447b64194e4dd16b69786229336c8725507
Sachin S <mail.sachinsankar@gmail.com>
2023-05-04 14:47:23 +0530

    Moved QuickProxy function from swiftshadow.py to __init__.py

--- swiftshadow/__init__.py
+++ swiftshadow/__init__.py
@@ -1,1 +1,19 @@
+from swiftshadow.providers import Proxyscrape, Scrapingant
 
+
+def QuickProxy(countries: list = [],protocol: str = 'http'):
+	'''
+	This function is a faster alternative to `Proxy` class.
+	No caching is done.
+	
+	Args:
+         countries: ISO 3166-2 Two letter country codes to filter proxies.
+         protocol: HTTP/HTTPS protocol to filter proxies.
+ 
+	Returns:
+			proxyObject (dict): A working proxy object.
+	'''
+	try:
+		return Proxyscrape(1,countries=countries,protocol=protocol)[0]
+	except:
+		return Scrapingant(1,countries=countries,protocol=protocol)[0]
