77bcc9f6797d3f9cc45188a8ba0e8abeb1977986
sachin-sankar <mail.sachinsankar@gmail.com>
2024-07-01 11:40:11 +0530

    chore: Removed redundant country check

--- swiftshadow/helpers.py
+++ swiftshadow/helpers.py
@@ -1,21 +1,8 @@
-from swiftshadow.constants import CountryCodes
 from requests import get
 from datetime import datetime
 
 
-def getCountryCode(countryName):
-    try:
-        return CountryCodes[countryName]
-    except KeyError:
-        for name in list(CountryCodes.keys()):
-            if countryName in name:
-                return CountryCodes[name]
-
-
-def checkProxy(proxy, countries):
-    if countries != []:
-        if proxy[-1].upper() not in countries:
-            return False
+def checkProxy(proxy):
     proxyDict = {proxy[1]: proxy[0]}
     try:
         resp = get(f"{proxy[1]}://ipinfo.io/ip", proxies=proxyDict, timeout=2).text
@@ -27,7 +14,7 @@ def checkProxy(proxy, countries):
         return False
 
 
-def log(level, message):
+def log(level, message):  
     level = level.upper()
     print(
         f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} - [swiftshadow] - {level} : {message}'
