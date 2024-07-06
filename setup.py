9ebd3bc902d695917afb5a2f7006218b7d2a19d6
Sachin Sankar <mail.sachinsankar@gmail.com>
2024-07-06 18:10:12 +0530

    chore: Bump lib version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     description="Free IP Proxy rotator for python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(where=".", exclude=["tests"]),
     install_requires=["requests"],
     classifiers=[
