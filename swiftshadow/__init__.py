0e48dfc4b98b017fd0ce68dcfc6382154cbc7a83
sachin-sankar <mail.sachinsankar@gmail.com>
2025-01-26 18:14:05 +0530

    feat!: Refactor QuickProxy to support async providers

--- swiftshadow/__init__.py
+++ swiftshadow/__init__.py
@@ -1,25 +1,30 @@
+from typing import Literal
 from swiftshadow.providers import Providers
+from swiftshadow.models import Proxy
+from asyncio import run
 
 
-def QuickProxy(countries: list = [], protocol: str = "http"):
+def QuickProxy(
+    countries: list[str] = [], protocol: Literal["http", "https"] = "http"
+) -> Proxy | None:
     """
     This function is a faster alternative to `Proxy` class.
     No caching is done.
 
     Args:
-     countries: ISO 3166-2 Two letter country codes to filter proxies.
-     protocol: HTTP/HTTPS protocol to filter proxies.
+        countries: ISO 3166-2 Two letter country codes to filter proxies.
+        protocol: HTTP/HTTPS protocol to filter proxies.
 
     Returns:
-                    proxyObject (dict): A working proxy object.
+        proxyObject (dict): A working proxy object if found or else None.
     """
-    for providerDict in Providers:
-        if protocol not in providerDict["protocols"]:
+    for provider in Providers:
+        if protocol not in provider.protocols:
             continue
-        if (len(countries) != 0) and (not providerDict["countryFilter"]):
+        if (len(countries) != 0) and (not provider.countryFilter):
             continue
         try:
-            return providerDict["provider"](1, countries, protocol)[0]
+            return run(provider.providerFunction(countries, protocol))[0]
         except Exception:
             continue
     return None
